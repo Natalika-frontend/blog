946b732c81a6afbb06d2953041ca5df864b4e9eb
Наталья Романова <Natalocka_ne@mail.ru>
2024-05-25 13:36:29 +0300

    зpost page implemented

--- src/bff/operations/fetch-post.js
+++ src/bff/operations/fetch-post.js
@@ -1,15 +1,25 @@
-import { getComments, getPost } from '../api';
+import { getComments, getPost, getUsers } from '../api';
 
 export const fetchPost = async (postId) => {
 	const post = await getPost(postId);
+	const users = await getUsers();
 
 	const comments = await getComments(postId);
+	// подход к получению логина пользователя выбран "неправильный", так как работаем c JSON-server. При существовании реальной базы данных на сервере должны делаться запросы, объединяющие таблицы по колонке user_id
+	const commentsWidthAuthor = comments.map((comment) => {
+		const user = users.find(({ id }) => id === comment.authorId);
+
+		return {
+			...comment,
+			author: user?.login,
+		};
+	});
 
 	return {
 		error: null,
 		res: {
 			...post,
-			comments,
+			comments: commentsWidthAuthor,
 		},
 	};
 };
--- src/pages/post/components/comments/components/comment.jsx
+++ src/pages/post/components/comments/components/comment.jsx
@@ -4,30 +4,44 @@ import { Icon } from '../../../../../components';
 const CommentContainer = ({ className, id, author, publishedAt, content }) => {
 	return (
 		<div className={className}>
-			<div className="informetiom-panel">
-				<div className="author">
-					<Icon
-						id="fa-user-circle-o"
-						margin="0 10px 0 0"
-						onClick={() => {}}
-					/>
-					{author}
+			<div className="comment">
+				<div className="information-panel">
+					<div className="author">
+						<Icon
+							id="fa-user-circle-o"
+							size="18px"
+							margin="0 10px 0 0"
+							onClick={() => {}}
+						/>
+						{author}
+					</div>
+					<div className="published-at">
+						<Icon
+							id="fa-calendar-o"
+							size="18px"
+							margin="0 10px 0 0"
+							onClick={() => {}}
+						/>
+						{publishedAt}
+					</div>
 				</div>
-				<div className="publiched-at">
-					<Icon
-						id="fa-calendar-o"
-						margin="0 10px 0 0"
-						onClick={() => {}}
-					/>
-					{publishedAt}
-				</div>
+				<div className="comment-text">{content}</div>
 			</div>
-			<div className="comment-text">{content}</div>
+			<Icon id="fa-trash-o" margin="0 0 0 10px" onClick={() => {}} />
 		</div>
 	);
 };
 
 export const Comment = styled(CommentContainer)`
+	display: flex;
+	margin: 10px 0 0;
+
+	& .comment {
+		width: 547px;
+		border: 1px solid #eeeeee;
+		padding: 10px;
+	}
+
 	& .information-panel {
 		display: flex;
 		justify-content: space-between;
@@ -37,7 +51,7 @@ export const Comment = styled(CommentContainer)`
 		display: flex;
 	}
 
-	& .publiched-at {
+	& .published-at {
 		display: flex;
 	}
 `;
